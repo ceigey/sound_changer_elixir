53ba40b1e7355e384a022c9671f485ab1473efeb
Christian <ceigey@users.noreply.github.com>
2018-01-13 21:49:00 +1030

    First commit

---
+++ lib/sound_changer.ex
@@ -0,0 +1,18 @@
+defmodule Sound_Changer do
+  @moduledoc """
+  Documentation for Sound_Changer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sound_Changer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sound_changer_test.exs
@@ -0,0 +1,8 @@
+defmodule Sound_ChangerTest do
+  use ExUnit.Case
+  doctest Sound_Changer
+
+  test "greets the world" do
+    assert Sound_Changer.hello() == :world
+  end
+end
